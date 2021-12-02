aecdc8c61cb32d2200bd43270bd002469a99c0cc
unknown <mihaelachira8@gmail.com>
2021-12-02 18:17:50 +0200

    added main menu and leaderboard

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Scoreboard()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
