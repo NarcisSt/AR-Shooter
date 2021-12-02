0468709f815df1c8d814dfaed30dc000839c0b20
Martonca Daniel <martonca.daniel@yahoo.com>
2021-12-02 17:10:36 +0200

    Added mock shooter

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public GameObject arCamera;
+
+    public AudioSource gunSound;
+
+    public AnimationClip deathClip;
+
+    public GameObject[] characters;
+
+    public void Shoot()
+    {
+        gunSound.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            var obj = hit.transform.gameObject;
+            if (obj.CompareTag("Enemy"))
+            {
+                Destroy(hit.transform.gameObject);
+                // var an = obj.GetComponent<Animation>();
+                // an.Play("Death");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] gameObjects;
+    public GameObject[] startAnimations;
+    private enum Character
+    {
+        Orc = 1,
+        Zombie = 3,
+        Alien = 0,
+        Princess = 2
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCharacter(Character.Orc);
+        SpawnCharacter(Character.Alien);
+        SpawnCharacter(Character.Zombie);
+        SpawnCharacter(Character.Princess);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator WaitNSeconds(int n)
+    {
+        yield return new WaitForSeconds(n);
+    }
+
+    void SpawnCharacter(Character character, int delay=0)
+    {
+        if (delay > 0)
+            StartCoroutine(WaitNSeconds(delay));
+
+        var i = (int) character;
+        var obj=Instantiate(gameObjects[i], spawnPoints[i].position, spawnPoints[i].rotation);
+
+        // obj.GetComponent<Animation>().Play(startAnimations[i].name);
+    }
+}
