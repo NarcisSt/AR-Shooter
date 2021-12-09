68ce8b702b9514a8a5e74347fe49113790b86196
unknown <mihaelachira8@gmail.com>
2021-12-09 22:53:05 +0200

    added vuforia and monsters spawning

---
+++ Assets/Scripts/KeepSize.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class KeepSize : MonoBehaviour
+{
+    void Update()
+    {
+        float dist = (Camera.main.transform.position - transform.position).magnitude / 3f;
+
+        transform.localScale = new Vector3(dist, dist, dist);
+    }
+}
