6f3514eec3c849b3136a9ce293e799c185b22425
unknown <mihaelachira8@gmail.com>
2021-12-10 11:16:21 +0200

    modified some canvas sizes and fixed the main menu for mobile

--- Assets/Scripts/KeepSize.cs
+++ Assets/Scripts/KeepSize.cs
@@ -4,7 +4,7 @@ public class KeepSize : MonoBehaviour
 {
     void Update()
     {
-        float dist = (Camera.main.transform.position - transform.position).magnitude / 3f;
+        float dist = (Camera.main.transform.position - transform.position).magnitude / 4f;
 
         transform.localScale = new Vector3(dist, dist, dist);
     }
